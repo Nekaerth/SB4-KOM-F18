1d2314328c2879481dbda81f1d926ba47d66c30b
Nekaerth <lasse.j.lundin@gmail.com>
2018-04-15 22:33:16 +0200

    Reset all OSGi

--- AsteroidsNetBeansOSGi/CommonAsteroid/src/main/java/dk/sdu/mmmi/cbse/commonasteroid/data/Asteroid.java
+++
@@ -1,7 +0,0 @@
-package dk.sdu.mmmi.cbse.commonasteroid.data;
-
-import dk.sdu.mmmi.cbse.common.data.Entity;
-
-public class Asteroid extends Entity {
-
-}
--- AsteroidsNetBeansOSGi/CommonPlayer/src/main/java/dk/sdu/mmmi/cbse/commonplayer/data/Player.java
+++
@@ -1,7 +0,0 @@
-package dk.sdu.mmmi.cbse.commonplayer.data;
-
-import dk.sdu.mmmi.cbse.common.data.Entity;
-
-public class Player extends Entity {
-
-}
